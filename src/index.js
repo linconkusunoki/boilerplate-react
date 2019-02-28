62f5cd77ac7d8add1d8d068119784c56abb36ace
Lincon Kusunoki <lincon.kusunoki@yapay.com.br>
2019-02-28 11:24:53 -0300

    Add Skipwaiting button to refresh PWA cache

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom'
 import Root from 'containers/Root'
 import configureStore from './store/configureStore'
 import { BrowserRouter as Router } from 'react-router-dom'
+import * as serviceWorker from './config/serviceWorker'
+import initWorkboxRefresh from '@loopmode/cra-workbox-refresh'
 
 const store = configureStore()
 
@@ -12,3 +14,8 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root'),
 )
+
+// add the refresh button to update pwa cache
+serviceWorker.register({
+  onUpdate: registration => initWorkboxRefresh(registration),
+})
